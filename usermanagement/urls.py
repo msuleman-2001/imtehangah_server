90d59161892a888d323b7a84d3e735a45a2e425c
NoorzaibMunawar <noorzaibmunawar@icloud.com>
2025-03-21 12:16:14 +0500

    Modified Created Models, Serilizers and Views for Users,Teacher,Student

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('users/', views.UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<uuid:pk>/', views.UserDetailView.as_view(), name='user-detail'),
+    path('students/', views.StudentListCreateView.as_view(), name='student-list-create'),
+    path('students/<uuid:pk>/', views.StudentDetailView.as_view(), name='student-detail'),
+    path('teachers/', views.TeacherListCreateView.as_view(), name='teacher-list-create'),
+    path('teachers/<uuid:pk>/', views.TeacherDetailView.as_view(), name='teacher-detail'),
 ]
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import BaseUser, Student, Teacher
+from .serializers import BaseUserSerializer, StudentSerializer, TeacherSerializer
+
 
 # Create your views here.
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = BaseUser.objects.all()
+    serializer_class = BaseUserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BaseUser.objects.all()
+    serializer_class = BaseUserSerializer
+
+
+class StudentListCreateView(generics.ListCreateAPIView):
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        return Student.objects.all()
+
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        return Student.objects.all()
+
+
+class TeacherListCreateView(generics.ListCreateAPIView):
+    serializer_class = TeacherSerializer
+
+    def get_queryset(self):
+        return Teacher.objects.all()
+
+
+class TeacherDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TeacherSerializer
+
+    def get_queryset(self):
+        return Teacher.objects.all()
